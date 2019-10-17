09f0611dcb7e80c02e3f61a5d99331da4b418e76
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-10-17 03:00:50 +0000

    more exercises completed

---
+++ 1_queries/2_queries.sql
@@ -0,0 +1,8 @@
+-- Our product managers want a query to see the average duration of a reservation.
+
+-- Get the average duration of all reservations.
+
+-- Expected Result:
+
+SELECT AVG(end_date - start_date) AS average_duration
+FROM reservations;
